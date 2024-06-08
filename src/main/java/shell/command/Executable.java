712318d63d4baf723a2de2c38dfc46c2552b7c9b
Caceresenzo <caceresenzo1502@gmail.com>
2024-06-08 16:01:48 +0200

    pass 8th stage

--- src/main/java/shell/command/Executable.java
+++ src/main/java/shell/command/Executable.java
@@ -1,14 +1,31 @@
 package shell.command;
 
 import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import lombok.SneakyThrows;
 
 public record Executable(
 	Path path
 ) implements Command {
 
+	@SneakyThrows
 	@Override
 	public void execute(String[] arguments) {
-		throw new UnsupportedOperationException();
+		try {
+			final var commandArguments = Stream
+				.concat(
+					Stream.of(path.toString()),
+					Arrays.stream(arguments).skip(1)
+				)
+				.toList();
+
+			final var process = new ProcessBuilder(commandArguments).start();
+			process.waitFor();
+		} catch (Exception exception) {
+			System.err.println(exception.getMessage());
+		}
 	}
 
 }
