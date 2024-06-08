1ff95c9457f4df5589b9c7b5096b18a328c98478
Caceresenzo <caceresenzo1502@gmail.com>
2024-06-08 15:32:59 +0200

    pass 4th stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,13 @@
+
+import java.util.Map;
 import java.util.Scanner;
+import java.util.function.Consumer;
 
 public class Main {
+
+	public static final Map<String, Consumer<String[]>> BUILTINS = Map.of(
+		"exit", (args) -> System.exit(0)
+	);
 
 	public static void main(String[] args) throws Exception {
 		try (Scanner scanner = new Scanner(System.in)) {
@@ -25,6 +32,12 @@ public class Main {
 	public static void eval(String line) {
 		final var arguments = line.split(" ");
 		final var program = arguments[0];
+
+		final var builtin = BUILTINS.get(program);
+		if (builtin != null) {
+			builtin.accept(arguments);
+			return;
+		}
 
 		System.out.println("%s: command not found".formatted(program));
 	}
