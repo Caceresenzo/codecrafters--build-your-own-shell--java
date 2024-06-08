9c78294a07fda4423de3279eae3e7cf7a608e938
Caceresenzo <caceresenzo1502@gmail.com>
2024-06-08 15:29:20 +0200

    pass 3rd stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,15 +3,30 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		System.out.print("$ ");
-
 		try (Scanner scanner = new Scanner(System.in)) {
-			final var input = scanner.nextLine();
-			final var arguments = input.split(" ");
-			final var program = arguments[0];
-
-			System.out.println("%s: command not found".formatted(program));
+			while (true) {
+				final var line = read(scanner);
+				eval(line);
+			}
 		}
+	}
+
+	public static String read(Scanner scanner) {
+		while (true) {
+			System.out.print("$ ");
+			final var line = scanner.nextLine();;
+
+			if (!line.isBlank()) {
+				return line;
+			}
+		}
+	}
+
+	public static void eval(String line) {
+		final var arguments = line.split(" ");
+		final var program = arguments[0];
+
+		System.out.println("%s: command not found".formatted(program));
 	}
 
 }
