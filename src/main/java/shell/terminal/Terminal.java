4bffda862b4b1b96584adff04c489f6f45de7135
Caceresenzo <caceresenzo1502@gmail.com>
2025-01-28 01:33:07 +0100

    CLI tests (cli-test-1738024387227)

---
+++ src/main/java/shell/terminal/Terminal.java
@@ -0,0 +1,48 @@
+package shell.terminal;
+
+import jtermios.JTermios;
+import jtermios.Termios;
+import lombok.SneakyThrows;
+
+public class Terminal implements AutoCloseable {
+
+	private static final int STDIN_FD = 0;
+	private static final int STDOUT_FD = 1;
+
+	private final Termios previous = new Termios();
+
+	public Terminal() {
+		JTermios.tcgetattr(STDIN_FD, previous);
+
+		Termios termios = new Termios();
+		JTermios.tcgetattr(STDIN_FD, termios);
+		termios.c_iflag &= JTermios.IGNCR;
+		termios.c_lflag ^= JTermios.ICANON;
+		termios.c_lflag ^= JTermios.ECHO;
+		termios.c_cc[JTermios.VMIN] = 1;
+		termios.c_cc[JTermios.VTIME] = 0;
+		JTermios.tcsetattr(STDIN_FD, JTermios.TCSANOW, termios);
+	}
+
+	@SneakyThrows
+	public char read() {
+		var value = System.in.read();
+
+		if (value < 0) {
+			value = 0;
+		}
+
+		return (char) value;
+	}
+
+	@Override
+	public void close() {
+		JTermios.tcsetattr(STDIN_FD, JTermios.TCSANOW, previous);
+	}
+
+	public static void write(String string) {
+		final var bytes = string.getBytes();
+		JTermios.write(STDOUT_FD, bytes, bytes.length);
+	}
+
+}
