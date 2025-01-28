bb44ea7355a1485652d8f03f62b271c59610b564
Caceresenzo <caceresenzo1502@gmail.com>
2025-01-28 23:35:49 +0100

    CLI tests (cli-test-1738103749083)

---
+++ src/main/java/shell/autocomplete/CompletionResolver.java
@@ -0,0 +1,12 @@
+package shell.autocomplete;
+
+import java.util.Set;
+
+import shell.Shell;
+
+@FunctionalInterface
+public interface CompletionResolver {
+
+	Set<String> getCompletions(Shell shell, String beginning);
+
+}
---
+++ src/main/java/shell/autocomplete/impl/BuiltinCompletionResolver.java
@@ -0,0 +1,22 @@
+package shell.autocomplete.impl;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import shell.Shell;
+import shell.autocomplete.CompletionResolver;
+
+public enum BuiltinCompletionResolver implements CompletionResolver {
+
+	INSTANCE;
+
+	@Override
+	public Set<String> getCompletions(Shell shell, String beginning) {
+		return shell.getBuiltins()
+			.keySet()
+			.stream()
+			.filter((name) -> name.startsWith(beginning))
+			.collect(Collectors.toSet());
+	}
+
+}
