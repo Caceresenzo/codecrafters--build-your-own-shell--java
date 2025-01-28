0f4cad8f6c87dc48510652cd358b67b05ffe6613
Caceresenzo <caceresenzo1502@gmail.com>
2025-01-28 02:00:21 +0100

    CLI tests (cli-test-1738026021730)

---
+++ src/main/java/shell/terminal/Terminal.java
@@ -0,0 +1,74 @@
+package shell.terminal;
+
+import java.io.IOException;
+
+import jtermios.JTermios;
+import jtermios.Termios;
+import lombok.SneakyThrows;
+
+public class Terminal implements AutoCloseable {
+
+	private static final int STDIN_FD = 0;
+	private static final int STDOUT_FD = 1;
+
+	private final Termios previous = new Termios();
+
+	public Terminal() {
+		//		checkErrno(JTermios.tcgetattr(STDIN_FD, previous));
+		//
+		//		Termios termios = new Termios();
+		//		termios.set(termios);
+		//
+		//		checkErrno(JTermios.tcgetattr(STDIN_FD, termios));
+		//		//		termios.c_iflag &= JTermios.IGNCR;
+		//		termios.c_lflag &= ~(JTermios.ECHO | JTermios.ICANON);
+		//		termios.c_cc[JTermios.VMIN] = 1;
+		//		termios.c_cc[JTermios.VTIME] = 0;
+		//		checkErrno(JTermios.tcsetattr(STDIN_FD, JTermios.TCSANOW, termios));
+
+		String[] cmd = { "/bin/sh", "-c", "stty -icanon -echo < /dev/tty" };
+		try {
+			Runtime.getRuntime().exec(cmd);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@SneakyThrows
+	public char read() {
+		var value = System.in.read();
+
+		if (value < 0) {
+			value = 0;
+		}
+
+		return (char) value;
+	}
+
+	@Override
+	public void close() {
+		//		checkErrno(JTermios.tcsetattr(STDIN_FD, JTermios.TCSANOW, previous));
+
+		String[] cmd = { "/bin/sh", "-c", "stty sane < /dev/tty" };
+		try {
+			Runtime.getRuntime().exec(cmd);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private static void checkErrno(int previousReturn) {
+		if (previousReturn != -1) {
+			return;
+		}
+
+		final var errno = JTermios.errno();
+		throw new IllegalStateException("errno: %s".formatted(errno));
+	}
+
+	public static void write(String string) {
+		final var bytes = string.getBytes();
+		JTermios.write(STDOUT_FD, bytes, bytes.length);
+	}
+
+}
