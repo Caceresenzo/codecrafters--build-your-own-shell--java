ab1387c61ecd4dd7027cc0ba1ab6f5e5bb0bda7f
Caceresenzo <caceresenzo1502@gmail.com>
2025-05-04 19:03:40 +0200

    CLI tests (cli-test-1746378220005)

--- src/main/java/shell/io/RedirectStreams.java
+++ src/main/java/shell/io/RedirectStreams.java
@@ -15,6 +15,44 @@ public record RedirectStreams(
 		try (output; error) {}
 	}
 
+	public void apply(ProcessBuilder builder) {
+		applyRedirect(builder, output, StandardNamedStream.OUTPUT);
+		applyRedirect(builder, error, StandardNamedStream.ERROR);
+	}
+
+	private void applyRedirect(ProcessBuilder builder, RedirectStream stream, StandardNamedStream streamName) {
+		final var isStderr = StandardNamedStream.ERROR.equals(streamName);
+
+		switch (stream) {
+			case RedirectStream.Standard standard -> {
+				if (isStderr && StandardNamedStream.OUTPUT.equals(standard.name())) {
+					builder.redirectErrorStream(true);
+				}
+			}
+
+			case RedirectStream.File file -> {
+				file.close();
+
+				final var redirect = file.append()
+					? ProcessBuilder.Redirect.appendTo(file.path().toFile())
+					: ProcessBuilder.Redirect.to(file.path().toFile());
+
+				if (isStderr) {
+					builder.redirectError(redirect);
+				} else {
+					builder.redirectOutput(redirect);
+				}
+			}
+		}
+	}
+
+	public static RedirectStreams standard() {
+		return new RedirectStreams(
+			new RedirectStream.Standard(StandardNamedStream.OUTPUT, System.out),
+			new RedirectStream.Standard(StandardNamedStream.ERROR, System.err)
+		);
+	}
+
 	public static RedirectStreams from(List<Redirect> redirects) throws FileNotFoundException {
 		RedirectStream output = new RedirectStream.Standard(StandardNamedStream.OUTPUT, System.out);
 		RedirectStream error = new RedirectStream.Standard(StandardNamedStream.ERROR, System.err);
--- src/main/java/shell/parse/ParsedCommand.java
+++ src/main/java/shell/parse/ParsedCommand.java
@@ -2,7 +2,13 @@ package shell.parse;
 
 import java.util.List;
 
-public record ParsedLine(
+public record ParsedCommand(
 	List<String> arguments,
 	List<Redirect> redirects
-) {}
+) {
+
+	public String program() {
+		return arguments.getFirst();
+	}
+
+}
