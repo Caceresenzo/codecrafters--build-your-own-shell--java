11c4c3c329ca9d3296a791b672e0a07b7532689b
Caceresenzo <caceresenzo1502@gmail.com>
2026-02-28 16:21:06 +0100

    pass 1st filename-completion

---
+++ src/main/java/shell/autocomplete/impl/FileCompletionResolver.java
@@ -0,0 +1,33 @@
+package shell.autocomplete.impl;
+
+import java.io.FileFilter;
+import java.util.HashSet;
+import java.util.Set;
+
+import shell.Shell;
+import shell.autocomplete.CompletionResolver;
+
+public enum FileCompletionResolver implements CompletionResolver {
+
+	INSTANCE;
+
+	@Override
+	public Set<String> getCompletions(Shell shell, boolean isCommand, String beginning) {
+		final FileFilter filter = (file) -> {
+			return file.getName().startsWith(beginning);
+		};
+
+		final var files = shell.getWorkingDirectory().toFile().listFiles(filter);
+		if (files == null) {
+			return Set.of();
+		}
+
+		final var candidates = new HashSet<String>();
+		for (final var file : files) {
+			candidates.add(file.getName());
+		}
+
+		return candidates;
+	}
+
+}
