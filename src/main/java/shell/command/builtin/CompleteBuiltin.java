8eade6bd3c66cc3338a34722e78b6cdda332a36c
Caceresenzo <caceresenzo1502@gmail.com>
2026-05-17 23:32:56 +0200

    CLI tests (cli-test-1779053576440)

--- src/main/java/shell/command/builtin/CompleteBuiltin.java
+++ src/main/java/shell/command/builtin/CompleteBuiltin.java
@@ -12,6 +12,20 @@ public enum CompleteBuiltin implements Builtin {
 
 	@Override
 	public OptionalInt execute(Shell shell, List<String> arguments, RedirectStreams redirectStreams, boolean isJob) {
+		final var completeProgramName = arguments.get(0);
+		final var flag = arguments.get(1);
+
+		switch (flag) {
+			case "-p" -> {
+				final var programName = arguments.get(2);
+				redirectStreams.error().printf("%s: %s: no completion specification%n", completeProgramName, programName);
+			}
+
+			default -> {
+				redirectStreams.error().printf("%s: unknown flag: %s%n", completeProgramName, flag);
+			}
+		}
+
 		return OptionalInt.empty();
 	}
 
