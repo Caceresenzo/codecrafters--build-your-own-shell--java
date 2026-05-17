8597a3ddf6810480fa4a4d018d6923e12371e8de
Caceresenzo <caceresenzo1502@gmail.com>
2026-05-17 23:27:42 +0200

    pass 1st programmable-completion

--- src/main/java/shell/Shell.java
+++ src/main/java/shell/Shell.java
@@ -10,6 +10,7 @@ import shell.command.Binary;
 import shell.command.Executable;
 import shell.command.builtin.Builtin;
 import shell.command.builtin.CdBuiltin;
+import shell.command.builtin.CompleteBuiltin;
 import shell.command.builtin.DeclareBuiltin;
 import shell.command.builtin.EchoBuiltin;
 import shell.command.builtin.ExitBuiltin;
@@ -33,7 +34,8 @@ public class Shell implements AutoCloseable {
 		"cd", CdBuiltin.INSTANCE,
 		"history", HistoryBuiltin.INSTANCE,
 		"jobs", JobsBuiltin.INSTANCE,
-		"declare", DeclareBuiltin.INSTANCE
+		"declare", DeclareBuiltin.INSTANCE,
+		"complete", CompleteBuiltin.INSTANCE
 	);
 
 	private @Getter Path workingDirectory = Path.of(".").toAbsolutePath().normalize();
